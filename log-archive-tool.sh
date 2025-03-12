aeb5a6006860b73ed2dbf289e350df132dc37e3f
JulianTs0 <juliantorresssr@gmail.com>
2025-03-12 12:09:24 -0300

    First commit

---
+++ log-archive-tool.sh
@@ -0,0 +1,11 @@
+## Follow these steps to complete the project.
+## 
+## 1. Create a new public repository on GitHub.
+## 
+## 2. Complete the project according to the requirements and push your code to the GitHub repository.
+## 
+## 3. Add a README file with instructions to run the project and the project page URL
+## 
+## 4. Once done, submit your solution to help the others learn and get feedback from the community.
+
+
