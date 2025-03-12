53b04ab010c36d5586d8f813beca35a224f988a0
JulianTs0 <juliantorresssr@gmail.com>
2025-03-12 17:04:42 -0300

    Implementation of the inits validations & Test to obtain the variable actual_date

--- log-archive-tool.sh
+++ log-archive-tool.sh
@@ -8,4 +8,41 @@
 ## 
 ## 4. Once done, submit your solution to help the others learn and get feedback from the community.
 
+#!/bin/bash
+
+if test $# -eq 0
+then
+ echo "AN ARGUMENT IS MISSING, PLEASE PROVIDE A LOG DIRECTORY"
+ exit
+elif test $# -gt 1
+then
+  echo "A SINGLE ARGUMENT MUST BE PROVIDED WITH THE LOG DIRECTORY"
+  exit
+fi
+
+if !(test -d $1)
+then
+  echo "THE ARGUMENT MUST BE A LOG DIRECTORY"
+  exit
+fi
+
+logs=`ls $1 | grep  "\.log"`
+
+if test `echo $logs | wc -w` -eq 0
+then
+  echo -e "THE LOG DIRECTORY DOES NOT HAVE  A SINGLE .log FILE, NOTHING TO ARCHIVE\nRETURNING TO THE TERMINAL"
+  exit
+fi
+
+for i in $logs
+do
+  echo $i
+done
+
+actual_date=`date +%Y%m%d_%H%M%S`
+
+
+
+
+
 
