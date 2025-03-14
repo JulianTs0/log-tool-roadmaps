ae6008771e992d2ca409504527574de5a16d7bab
JulianTs0 <juliantorresssr@gmail.com>
2025-03-14 16:34:45 -0300

    Updated the README file & Removed the comentary lines in the main script

--- log-archive-tool.sh
+++ log-archive-tool.sh
@@ -1,13 +1,3 @@
-## Follow these steps to complete the project.
-## 
-## 1. Create a new public repository on GitHub.
-## 
-## 2. Complete the project according to the requirements and push your code to the GitHub repository.
-## 
-## 3. Add a README file with instructions to run the project and the project page URL
-## 
-## 4. Once done, submit your solution to help the others learn and get feedback from the community.
-
 #!/bin/bash
 
 if test $# -eq 0
